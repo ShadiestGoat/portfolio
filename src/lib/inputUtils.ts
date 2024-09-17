0769d5fee34a8e5803f841ae473e771654979b14
ShadiestGoat <48590492+ShadiestGoat@users.noreply.github.com>
2024-09-18 00:54:52 +0100

    svelte 5... when t-t

--- src/lib/inputUtils.ts
+++ src/lib/inputUtils.ts
@@ -13,17 +13,21 @@ export function shouldContinueEvent(e: AnyInputEvent): boolean {
 	return false
 }
 
-export function inputWrapper(h: (e: AnyInputEvent) => void): Record<'on:click' | 'on:keypress', ((e: AnyInputEvent) => void)> {
-	const f = function (e: AnyInputEvent) {
+export function mkInputHandler(h: (e: AnyInputEvent) => void): ((e: AnyInputEvent) => void) {
+	return (e) => {
 		if (!shouldContinueEvent(e)) {
 			return
 		}
 
 		h(e)
 	}
-
-	return {
-		"on:click": f,
-		"on:keypress": f,
-	}
 }
+
+// Sigh... waiting for svelte 5
+// export function inputWrapper(h: (e: AnyInputEvent) => void): Record<'on:click' | 'on:keypress', ((e: AnyInputEvent) => void)> {
+// 	const f = mkInputHandler(h)
+// 	return {
+// 		"on:click": f,
+// 		"on:keypress": f,
+// 	}
+// }
